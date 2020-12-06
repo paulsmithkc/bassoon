028a54c51c8637df6b0b37467d10c80f7e9757c7
Paul Smith <paulsmithkc@gmail.com>
2020-12-05 18:42:17 -0600

    implmenting chunking for better perfomance with large result sets

--- src/bassoon.js
+++ src/bassoon.js
@@ -17,6 +17,20 @@ export default function bassoon(arg1) {
     let stack = [];
     let curObj = null;
     let curKey = null;
+    let chunk = [];
+    let chunkSize = args.chunkSize;
+
+    function emitData(data) {
+      if (chunkSize) {
+        chunk.push(data);
+        if (chunk.length >= chunkSize) {
+          emitter.emit('data', chunk);
+          chunk = [];
+        }
+      } else {
+        emitter.emit('data', data);
+      }
+    }
 
     parser.callback = function ({ key, data, depth }) {
       //console.log(' '.repeat(depth * 2), key, data);
@@ -45,7 +59,7 @@ export default function bassoon(arg1) {
                 curObj.push(data);
               }
             } else {
-              emitter.emit('data', curObj);
+              emitData(curObj);
               curObj = curKey = null;
             }
             break;
@@ -72,7 +86,7 @@ export default function bassoon(arg1) {
             curObj = {};
             break;
           case 'value':
-            emitter.emit('data', data);
+            emitData(data);
             break;
         }
       }
@@ -89,16 +103,21 @@ export default function bassoon(arg1) {
           const fullText = xhr.responseText;
           const newText = fullText.substr(seen);
           seen = fullText.length;
-          // emitter.emit('text', newText);
           parser.parse(newText);
         }
       };
       xhr.onload = function (event) {
+        // let the parser terminate any partial values
         parser.end();
+        // emit the last object, even if it is incomplete
         if (stack.length) {
-          emitter.emit('data', stack[0]);
+          emitData(stack[0]);
         } else if (curObj) {
-          emitter.emit('data', curObj);
+          emitData(curObj);
+        }
+        // emit the last chunk
+        if (chunk.length) {
+          emitter.emit('data', chunk);
         }
         emitter.emit('end', {
           status: xhr.status,
